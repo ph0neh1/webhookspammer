29d637126434c4fad36bdfa8ed06920549b4c95f
Ph0neh1 <106416500+ph0neh1@users.noreply.github.com>
2023-08-19 17:26:41 -0600

    Add files via upload

---
+++ folder/main.py
@@ -0,0 +1,19 @@
+from discord import SyncWebhook
+import time
+
+print("Discord Webhook spammer by cr.ghost")
+time.sleep(5)
+
+webhook=input("Insert Webhook: ")
+webhook = SyncWebhook.from_url(webhook)
+msg=input("Insert Message: ")
+sleep=int(input('Sleeping time: '))
+amountofmessages=int(input('Number of messages: '))
+fakecount=0
+
+for x in range(amountofmessages):
+ fakecount=fakecount+1
+ webhook.send(msg)
+ current_time = time.strftime("%H:%M:%S", time.localtime())
+ print("<{}> Sent {}/{} messages".format(current_time, fakecount, amountofmessages))
+ time.sleep(sleep)
